540b857c0695f810f83d65e70d254ff8f2e25a68
barisonr <brsoner12@gmail.com>
2022-07-06 21:22:18 +0300

    İlk Commit

--- src/Grup14Sayim.java
+++ src/Grup14Sayim.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Grup14Sayim {
+import java.util.ArrayList;
+
+public class Grup14Sayim {
+    public static void main(String[] args) {
+        ArrayList<String> grupUyeleri = new ArrayList<>();
+
+        // Adınızı buraya ekleyiniz
+        grupUyeleri.add("Barış Öner");
+
+        for (String s : grupUyeleri) {
+            System.out.println(s);
+        }
+    }
 }
